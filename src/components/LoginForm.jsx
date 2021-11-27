f6dd743e8652ddfe4041edb786bd8f9230e133e5
ChandelAnushka <chandelanushka99@gmail.com>
2021-11-27 22:42:23 +0530

    README.md updated

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -29,13 +29,13 @@ const LoginForm = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Chat Application</h1>
+        <h1 className="title">Mentoring.io</h1>
         <form onSubmit={handleSubmit}>
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
             <button type="submit" className="button">
-              <span>Start chatting</span>
+              <span>Get Set Go!!</span>
             </button>
           </div>
         </form>
